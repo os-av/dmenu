004fba295453f0edd0a1412cd471d97877daaf32
os-av <os.av@tutanota.com>
2023-03-04 08:09:42 +0000

    text size update

--- config.def.h
+++ config.def.h
@@ -6,20 +6,20 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"terminus:style=Regular:size=12"
+	"terminus:style=Regular:size=18"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#e5e5e5", "#111111" },
-	[SchemeSel] = { "#e5e5e5", "#292929" },
+	[SchemeNorm] = { "#e5e5e5", "#000000" },
+	[SchemeSel] = { "#e5e5e5", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 28;
-static unsigned int min_lineheight = 28;
+static unsigned int lineheight = 40;
+static unsigned int min_lineheight = 40;
 
 /*
  * Characters not considered part of a word while deleting words
